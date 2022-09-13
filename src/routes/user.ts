15ce2f4f23cd4781bbe92de7d3418e7819f5cd4e
Elias <e.wambugu192@gmail.com>
2022-09-13 12:22:39 -0700

    finished top-tracks route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -24,4 +24,26 @@ router.get('/me', (req: express.Request, res: express.Response) => {
   }
 })
 
+//get top tracks
+router.get('/top-tracks', (req: express.Request, res: express.Response) => {
+  const access_token = req.query.access_token || null;
+  if (access_token || access_token !== 'undefined') {
+    var options = {
+      url: "https://api.spotify.com/v1/me/top/tracks",
+      headers: { Authorization: "Bearer " + access_token },
+      json: true,
+    };
+    
+    request.get(options, (error, _response, body) => {
+      if (error) {
+        res.status(500).json({ error: "An error has occurred" });
+      } else {
+        res.status(200).json(body);
+      }
+    });
+  } else {
+    res.status(401).json({ message: "Not logged in" });
+  }
+})
+
 export default router
